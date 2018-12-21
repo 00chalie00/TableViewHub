f36e153d2025e57a6d75fe39af8e0f3fe41dac9a
Lee Chul <winfe79@hanmail.net>
2018-12-21 12:13:03 +0900

    collectionViewcell added

---
+++ TableView/View/ProductVC.swift
@@ -0,0 +1,18 @@
+//
+//  ProductVC.swift
+//  TableView
+//
+//  Created by formathead on 21/12/2018.
+//  Copyright © 2018 formathead. All rights reserved.
+//
+
+import UIKit
+
+class ProductVC: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
