270a358700f45fd1f70c88073ff4541f228ca343
Lee Chul <winfe79@hanmail.net>
2019-01-02 16:27:27 +0900

    Storyboard ony

--- TableView/Controller/MainVC.swift
+++ TableView/Controller/MainVC.swift
@@ -8,46 +8,9 @@
 
 import UIKit
 
-class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    @IBOutlet weak var categoryTable: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        categoryTable.dataSource = self
-        categoryTable.delegate = self
-        
-        
-    }
-
-
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return ServiceData.instance.getCategory().count
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
-            
-            let category = ServiceData.instance.getCategory()[indexPath.row]
-            
-            cell.updateCategory(category: category)
-            
-            return cell
-            
-            
-        } else {
-            return CategoryCell()
-        }
-        
-        
-    }
+class MainVC: UIViewController {
     
     
-
+    
 }
 
